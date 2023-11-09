01a862c20521c17424de6e066aec1bbc3d441e9d
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2023-11-09 23:47:12 +0100

    Add files via upload

---
+++ domasno1.js
@@ -0,0 +1,58 @@
+// 1.
+
+let hasCoffe = true
+let hasTea = false
+let hasMilk = true
+// a. let resultOR = hasTea || hasMilk
+//console.log(resultOR)
+
+// b. let resultBool = hasCoffe && hasTea
+//console.log(resultBool)
+
+// c. let hasTea1 = true
+//console.log(!hasTea)
+
+// d. console.log(hasCoffe || hasMilk) && !(hasCofee && hasMilk )
+
+// e. console.log (hasTea && hasMilk) || (hasCoffe && hasTea)
+
+// f. console.log!(hasCoffe || hasTea)
+
+
+
+// 2. const num1 = 10;
+//const num2 = 5;
+//const num3 = 8;
+//const num4 = 2;
+//const num5 = 12;
+//const num6 = 4;
+//const num7 = 3;
+
+//console.log(num5 - num1)
+//console.log(num2 + num3)
+//console.log(num6 * num4)
+//console.log((num1 + num4) / num7)
+//console.log((num5 * num6) / num4)
+
+
+
+// 3.
+let num1 = 12;
+let num2 = 4;
+console.log num1 + num2
+console.log num1 - num2
+console.log num1 * num2
+console.log num1 / num2
+
+
+
+
+
+
+
+
+
+
+
+
+
