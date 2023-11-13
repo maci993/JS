7af1b777f8e3e5e65dbf68bafeed7a46fbd55850
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2023-11-13 22:29:45 +0100

    Update domasno3.js

--- domasno3.js
+++ domasno3.js
@@ -1,256 +1,249 @@
 //Задача 1.
 //а.
-//let fruits = ['pear', 'peach', 'plum'];
-//let fruitsLength = fruits.length
-//console.log(fruitsLength)
+let fruits = ['pear', 'peach', 'plum'];
+let fruitsLength = fruits.length
+console.log(fruitsLength)
 
 //1.
-//let firstElementOfFruits = fruits[1]
-//console.log(firstElementOfFruits)
+let firstElementOfFruits = fruits[1]
+console.log(firstElementOfFruits)
 
 //2.
-//fruits.push('orange');
-//console.log(fruits)
+fruits.push('orange');
+console.log(fruits)
 
 //б.
-//let person = {
-   // name: 'Marina Stojanovic',
-   // age: 29,
-   // city: 'Skopje',
-//}
+let person = {
+    name: 'Marina Stojanovic',
+    age: 29,
+    city: 'Skopje',
+}
 //1.
-//console.log(person.city)
+console.log(person.city)
 
 //2.
-//person.age = 30;
-//console.log(person)
+person.age = 30;
+console.log(person)
 
 //Задача 2.
 //a.
-//for (let i = 0; i <= 10; i++) {
-   // console.log(i);
-//}
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
 
 //б.
-//let start = 3;
-//let end = 10;
-//let sum1 = 0;
+let start = 3;
+let end = 10;
+let sum1 = 0;
 
-//for(let i=start; i<=end; i++) {
-   // if (i % 2 === 0)
-   // sum1 += i;
-//}
-//console.log(sum)
+for(let i=start; i<=end; i++) {
+    if (i % 2 === 0)
+    sum1 += i;
+}
+console.log(sum)
 
 //в.
-//let person1 = {
-    //name: 'Marina Stojanovic',
-    //age: 29,
-    //city: 'Skopje',
-//}
-//for (let property in person1) {
-  // console.log(property + ':' + person1[property]);
-//}
+let person1 = {
+    name: 'Marina Stojanovic',
+    age: 29,
+    city: 'Skopje',
+}
+for (let property in person1) {
+   console.log(property + ':' + person1[property]);
+}
 
 //г.
-//let colors = ['blue', 'pink', 'purple']
-//for (let color of colors) {
-   // console.log(color);
-//}
+let colors = ['blue', 'pink', 'purple']
+for (let color of colors) {
+    console.log(color);
+}
 
-//Задача 3.????
+//Задача 3.
 //a.
-//let count = 5;
-//let n = 1;
-
-//while (count <= n ) {
-  //  console.log(count);
-    //count += 1;
-//}
+let count = 5;
+while (count >=1 ) {
+    console.log(count);
+    count--;
+}
 
 
-// б.????
-//let randomNum = 10;
-//let randomNum1 = 1;
-//do {
-  //  sum += randomNum
-  //  randomNum++
-//} while (randomNum < 5)
-
-//console.log("All numbers:", sum )
+// б.
+let randomNum = 10;
+do { console.log(randomNum)
+    randomNum--
+  } while (randomNum >= 5)
 
 //Задача 4.
 //а.
-//let multiply = (x, y) => x * y;
-//let product = multiply(4, 5);
-//console.log(product)
+let multiply = (x, y) => x * y;
+let product = multiply(4, 5);
+console.log(product)
 
 
 //б.
-//let divide = (x, y) => x / y;
-//let product1 = divide(25, 5)
-//let result1 = 0;
-//console.log(product1)
+let divide = (x, y) => x / y;
+let product1 = divide(25, 5)
+let result1 = 0;
+console.log(product1)
 
 //в.
-//let arrowFunction = (a, b) => a * b;
-//let product2 = arrowFunction(6, 8);
-//let result2 = 0;
-//console.log(product2)
+let arrowFunction = (a, b) => a * b;
+let product2 = arrowFunction(6, 8);
+let result2 = 0;
+console.log(product2)
 
 //Задача 5.
-//function calculateArraySum(num1, num2, num3) {
-   // let sum = num1 + num2 + num3;
-    //return sum;
-//}
-//let result4 = calculateArraySum(6, 5, 2);
-//console.log(result4)
+function calculateArraySum(num1, num2, num3) {
+    let sum = num1 + num2 + num3;
+    return sum;
+}
+let result4 = calculateArraySum(6, 5, 2);
+console.log(result4)
 
-//Задача 6.????
-//let students = {
-  //  name: 'Mario',
-    //surname: 'Petkovski',
-    //age: 24,
-    //addess: {
-      //  city: 'Skopje',
-        //street: {
-          //  name1: 'Dimo Hadzi Dimov',
-            //number: 90,
-           // zipCode: '1000'
-        //}
-    //}
-//}
-//console.log(students.addess.city);
-//console.log(students["name1"])
+//Задача 6.
+let students = {
+    name: 'Mario',
+    surname: 'Petkovski',
+    age: 24,
+    addess: {
+        city: 'Skopje',
+        street: {
+            name1: 'Dimo Hadzi Dimov',
+            number: 90,
+            zipCode: '1000'
+        }
+   }
+}
+console.log(students.addess.city);
+console.log(students["name1"])
 
 //Задача 7.
 //a.
 
-  //   let number = 15;
-    // if (number % 3 == 0) {
-      //  console.log("Fizz")
-     //}
-     //if (number % 5 == 0  && !number % 3 == 0 ) {
-       // console.log("Buzz")
-     //}
-     //if (number % 5 == 0 && number % 3 == 0) {
-      //  console.log("FizzBuzz")
-     //}
+ let number = 15;
+ if (number % 3 == 0) {
+        console.log("Fizz")
+ }
+ if (number % 5 == 0  && !number % 3 == 0 ) {
+    console.log("Buzz")
+ }
+if (number % 5 == 0 && number % 3 == 0) {
+   console.log("FizzBuzz")
+}
 
 //б.
 
-//let month = "January";
-//switch (month) {
-  //  case "31":
-    //    console.log("January");
-      //  break;
+let month = "January";
+switch (month) {
+    case "January":
+        console.log("31");
+        break;
 
-    //case "28":
-      //  console.log("February");
-        //break;
+    case "February":
+        console.log("28");
+        break;
 
-    //case "31":
-      //  console.log("March");
-        //break;
+    case "March":
+       console.log("31");
+        break;
 
-    //case "30":
-      //  console.log("April");
-        //break;
+    case "April":
+        console.log("30");
+        break;
     
-    //case "31":
-      //  console.log("May");
-        //break;
+    case "May":
+        console.log("31");
+        break;
     
-    //case"30":
-      //  console.log("June");
-        //break;
+    case"June":
+        console.log("30");
+        break;
 
-    //case "31":
-      //  console.log("July");
-        //break;
+    case "July":
+        console.log("31");
+        break;
 
-    //case "31":
-      //  console.log("August")
-        //break;
+    case "August":
+        console.log("31")
+        break;
 
-    //case "30":
-      //  console.log("September")
-        //break;
+    case "September":
+       console.log("30")
+        break;
     
-    //case "31":
-      //  console.log("October")
-        //break;
+    case "October":
+        console.log("31")
+        break;
 
-    //case "30":
-      //  console.log("November")
-        //break;
+    case "November":
+        console.log("30")
+        break;
 
-    //case "31":
-      //  console.log("December")
-        //break;
-    //default:
-      //  console.log("Invalid input")
-
-//}
+    case "December":
+        console.log("31")
+        break;
+    default:
+        console.log("Invalid input")
+}
 
 //Задача 8.
 
 //а.
-//let book = {
-  //  name: "The Hunger Games",
-    //author: "Suzanne Collins",
-    //country: { 
-      //  name: "United States",
-       // language: "English",
-        //year: "2008"
-    //}
+let book = {
+    name: "The Hunger Games",
+    author: "Suzanne Collins",
+    country: { 
+        name: "United States",
+        language: "English",
+        year: "2008"
+    }
 
-//}
-//console.log(book)
+}
+console.log(book)
 
 //б.
 
-//let car = {
-  //  name: "Citroen C4",
-    //production: {
-      //  year: "2004",
-        //country: "France",
-        //automaker: "PSA Group",
-    //},
-//chronology: {
-  //  Predecesor: "Citroen Xsara",
-    //Successor: "DS 4"
-//},
-//class: "Smaal family car"
-//} 
+let car = {
+    name: "Citroen C4",
+    production: {
+        year: "2004",
+        country: "France",
+        automaker: "PSA Group",
+    },
+chronology: {
+    Predecesor: "Citroen Xsara",
+    Successor: "DS 4"
+},
+class: "Smaal family car"
+} 
     
-//console.log(car)
+console.log(car)
 
 //в.
-//let student = {
-  //  name: "Jane",
-    //surname: "Austen",
-    //faculty: {
-      //  name: "Faculty of Philosophy",
-        //department: "Psychology",
-        //year: "3rd"
-    //},
-    //city: "Washington DC",
-    //zipCode: "20003"
+let student = {
+    name: "Jane",
+    surname: "Austen",
+    faculty: {
+        name: "Faculty of Philosophy",
+        department: "Psychology",
+        year: "3rd"
+    },
+    city: "Washington DC",
+    zipCode: "20003"
 
-//}
-//console.log(student)
+}
+console.log(student)
 
-//Задача 9.???
-//а.
+//Задача 9.
+//а.???
 
 
 
 ////б.
-//for (let i = 1; i <=100; i++) {
-  //  if ( i % 2 !==0)
-    //console.log(i)
-//}
+for (let i = 1; i <=100; i++) {
+    if ( i % 2 !==0)
+    console.log(i)
+}
 
 
 
