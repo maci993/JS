ada4d120b78578100dd43b3899abef2bf3d25e51
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2023-11-13 11:41:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,78 +1,270 @@
-// 1.a.
-let globalNum = 10;
-function checkScope() {
-  console.log(globalNum);
-}
-checkScope();
+//Задача 1.
+//а.
+//let fruits = ['pear', 'peach', 'plum'];
+//let fruitsLength = fruits.length
+//console.log(fruitsLength)
 
-//б..
-let localNum = 10;
-function checkScope() {}
-console.log(globalNum === localNum);
+//1.
+//let firstElementOfFruits = fruits[1]
+//console.log(firstElementOfFruits)
 
-//в.
-let blockNum = 10;
-if (true) {
-}
-console.log(typeof blockNum);
-
-//2.a. ???
-let number = 10;
-if (number % 10 == 0) {
-  console.log("10 is even number");
-} else if (number % 10 == 0) {
-  console.log("10 is odd number");
-}
+//2.
+//fruits.push('orange');
+//console.log(fruits)
 
 //б.
-let age = 18;
-if (age === 18) {
-  console.log("Licnosta ima pravo da glasa");
-} else if (number < 18) {
-  console.log("Licnosta nema pravo da glasa");
-}
+//let person = {
+   // name: 'Marina Stojanovic',
+   // age: 29,
+   // city: 'Skopje',
+//}
+//1.
+//console.log(person.city)
+
+//2.
+//person.age = 30;
+//console.log(person)
+
+//Задача 2.
+//a.
+//for (let i = 0; i <= 10; i++) {
+   // console.log(i);
+//}
+
+//б.
+//let start = 3;
+//let end = 10;
+//let sum1 = 0;
+
+//for(let i=start; i<=end; i++) {
+   // if (i % 2 === 0)
+   // sum1 += i;
+//}
+//console.log(sum)
 
 //в.
-let score = 78;
-if (score === 78) {
-  console.log("Grade:C");
-} else if (score > 100) {
-  console.log("Invalid score! Please enter a score between 0 and 100");
-} else score < 0;
-console.log("Invalid score! Please enter a score between 0 and 100");
+//let person1 = {
+    //name: 'Marina Stojanovic',
+    //age: 29,
+    //city: 'Skopje',
+//}
+//for (let property in person1) {
+  // console.log(property + ':' + person1[property]);
+//}
 
 //г.
-let dayNumber = "2";
-switch (dayNumber) {
-  case "2":
-    console.log("The day is Monday");
-    break;
-  case "error":
-    console.log("Invalid day number. Please enter a number between 1 and 7.");
-}
+//let colors = ['blue', 'pink', 'purple']
+//for (let color of colors) {
+   // console.log(color);
+//}
 
-//3. 
-function addThreeNumbers(number1, number2, number3) {
-  let sum = number1 + number2 + number3;
-  return sum;
-}
-let result = addThreeNumbers(5,25,13)
-console.log(result);
+//Задача 3.????
+//a.
+//let count = 5;
+//let n = 1;
 
-//
-function printMyBirthDate(){
-    return "25.12.1993."
-}
-    console.log(printMyBirthDate())
+//while (count <= n ) {
+  //  console.log(count);
+    //count += 1;
+//}
+
+
+// б.????
+//let randomNum = 10;
+//let randomNum1 = 1;
+//do {
+  //  sum += randomNum
+  //  randomNum++
+//} while (randomNum < 5)
+
+//console.log("All numbers:", sum )
+
+//Задача 4.
+//а.
+//let multiply = (x, y) => x * y;
+//let product = multiply(4, 5);
+//console.log(product)
+
+
+//б.
+//let divide = (x, y) => x / y;
+//let product1 = divide(25, 5)
+//let result1 = 0;
+//console.log(product1)
+
+//в.
+//let arrowFunction = (a, b) => a * b;
+//let product2 = arrowFunction(6, 8);
+//let result2 = 0;
+//console.log(product2)
+
+//Задача 5.
+//function calculateArraySum(num1, num2, num3) {
+   // let sum = num1 + num2 + num3;
+    //return sum;
+//}
+//let result4 = calculateArraySum(6, 5, 2);
+//console.log(result4)
+
+//Задача 6.????
+//let students = {
+  //  name: 'Mario',
+    //surname: 'Petkovski',
+    //age: 24,
+    //addess: {
+      //  city: 'Skopje',
+        //street: {
+          //  name1: 'Dimo Hadzi Dimov',
+            //number: 90,
+           // zipCode: '1000'
+        //}
+    //}
+//}
+//console.log(students.addess.city);
+//console.log(students["name1"])
+
+//Задача 7.
+//a.
+
+  //   let number = 15;
+    // if (number % 3 == 0) {
+      //  console.log("Fizz")
+     //}
+     //if (number % 5 == 0  && !number % 3 == 0 ) {
+       // console.log("Buzz")
+     //}
+     //if (number % 5 == 0 && number % 3 == 0) {
+      //  console.log("FizzBuzz")
+     //}
+
+//б.
+
+//let month = "January";
+//switch (month) {
+  //  case "31":
+    //    console.log("January");
+      //  break;
+
+    //case "28":
+      //  console.log("February");
+        //break;
+
+    //case "31":
+      //  console.log("March");
+        //break;
+
+    //case "30":
+      //  console.log("April");
+        //break;
     
-// ???
- function printNum (num1, num2){
-    if(typeof num1 === num2){
-        if(num1 != num2){
-            console.log("These numbers are equal!")
-        }else{
-            console.log("These numbers are not equal")
-        }
-    }
- }
+    //case "31":
+      //  console.log("May");
+        //break;
+    
+    //case"30":
+      //  console.log("June");
+        //break;
+
+    //case "31":
+      //  console.log("July");
+        //break;
+
+    //case "31":
+      //  console.log("August")
+        //break;
+
+    //case "30":
+      //  console.log("September")
+        //break;
+    
+    //case "31":
+      //  console.log("October")
+        //break;
+
+    //case "30":
+      //  console.log("November")
+        //break;
+
+    //case "31":
+      //  console.log("December")
+        //break;
+    //default:
+      //  console.log("Invalid input")
+
+//}
+
+//Задача 8.
+
+//а.
+//let book = {
+  //  name: "The Hunger Games",
+    //author: "Suzanne Collins",
+    //country: { 
+      //  name: "United States",
+       // language: "English",
+        //year: "2008"
+    //}
+
+//}
+//console.log(book)
+
+//б.
+
+//let car = {
+  //  name: "Citroen C4",
+    //production: {
+      //  year: "2004",
+        //country: "France",
+        //automaker: "PSA Group",
+    //},
+//chronology: {
+  //  Predecesor: "Citroen Xsara",
+    //Successor: "DS 4"
+//},
+//class: "Smaal family car"
+//} 
+    
+//console.log(car)
+
+//в.
+//let student = {
+  //  name: "Jane",
+    //surname: "Austen",
+    //faculty: {
+      //  name: "Faculty of Philosophy",
+        //department: "Psychology",
+        //year: "3rd"
+    //},
+    //city: "Washington DC",
+    //zipCode: "20003"
+
+//}
+//console.log(student)
+
+//Задача 9.???
+//а.
+
+
+
+////б.
+//for (let i = 1; i <=100; i++) {
+  //  if ( i % 2 !==0)
+    //console.log(i)
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
